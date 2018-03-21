64979445e2eee7ccc8ac995cb02afdbffe67f8d6
Dermot Hardy <dermot.hardy@hpe.com>
2018-03-21 23:57:23 +0000

    Updated license header

--- src/main/docker/startup/startup.d/check-create-pgdb.sh
+++ src/main/docker/startup/startup.d/check-create-pgdb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-#
-# Copyright 2015-2018 EntIT Software LLC, a Micro Focus company.
+# Copyright 2015-2017 EntIT Software LLC, a Micro Focus company.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 #
 # This script is intended to facilitate database creation so that a later
 # running process can safely assume the configured database exists.

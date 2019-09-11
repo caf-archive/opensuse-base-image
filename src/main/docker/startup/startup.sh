f9729191ea86c2072b1dbd342536a1756250451b
thilagasanthosh <thilagavathi.santhoshkumar@microfocus.com>
2019-09-11 19:05:18 +0100

    SCMOD-7472: Space formatted

--- src/main/docker/startup/startup.sh
+++ src/main/docker/startup/startup.sh
@@ -25,7 +25,7 @@ log "Running startup scripts..."
 for script in $(dirname "$0")/startup.d/*; do
     if [ -x "$script" ]; then
         log "Running ${script##*/}..."
-        "$script" |& sed -ure "/^(info|error|warn|debug|trace|INFO|ERROR|WARN|DEBUG|TRACE):/!s/^/info: /I; s/^(([^:]*): ?)?(.*)$/[$(date +%H:%M:%S.%3NZ) #$(printf '%03X\n' $BASHPID).??? $(printf '%s\n' '\U\2\E')  -            - ]  ${script##*/}: \3/I"
+        "$script" |& sed -ure "/^(info|error|warn|debug|trace|INFO|ERROR|WARN|DEBUG|TRACE):/!s/^/info: /I; s/^(([^:]*): ?)?(.*)$/[$(date +%H:%M:%S.%3NZ) #$(printf '%03X\n' $BASHPID).??? $(printf '%s\n' '\U\2\E') -            -   ] ${script##*/}: \3/I"
     fi
 done
 

87bfb02f2696f56c8f55359e079772f20e5ed43e
Xbreizh <xavier.lamourec@gmail.com>
2022-03-23 12:25:25 +0000

    Update logging pattern

--- src/main/docker/startup/startup.sh
+++ src/main/docker/startup/startup.sh
@@ -17,7 +17,7 @@
 
 # Create a convenience function for logging
 log() {
-    echo "[$(date +%H:%M:%S.%3NZ) #$(printf '%03X\n' $$).??? INFO  -            -   ] ${0##*/}: $@"
+    echo "[$(date +%F\ %H:%M:%S.%3NZ) #$(printf '%03X\n' $$).??? INFO  -            -   ] ${0##*/}: $@"
 }
 
 # Export file based secrets
